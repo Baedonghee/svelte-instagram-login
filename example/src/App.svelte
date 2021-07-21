148a41ca85a1b1b0829389fab35c1e6624c7d2e0
Baedonghee <appleehdgml@gmail.com>
2021-07-22 00:21:44 +0900

    add instagram api

---
+++ example/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import InstagramLogin from '../../src/index.svelte';
+
+	const handleSuccess = () => {
+
+	}
+</script>
+
+<InstagramLogin 
+	clientId="your_client_id" 
+	redirectUrl="your_redirect_url" 
+	on:success={handleSuccess}
+	customRender>
+	<button>Custom instagram button</button>
+</InstagramLogin>
+
+<InstagramLogin />
